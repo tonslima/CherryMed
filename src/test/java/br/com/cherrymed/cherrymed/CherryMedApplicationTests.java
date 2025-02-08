af7c697fa687e53fce5ed3028a5fd5f4c3ba1bae
ton-mac <ton.slima@icloud.com>
2025-02-08 17:06:05 -0300

    first commit

---
+++ src/test/java/br/com/cherrymed/cherrymed/CherryMedApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cherrymed.cherrymed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CherryMedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
