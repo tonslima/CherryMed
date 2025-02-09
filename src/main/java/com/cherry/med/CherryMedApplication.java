6f65737caf5bd028fa73723d18916480c43c6633
ton-mac <ton.slima@icloud.com>
2025-02-09 11:44:03 -0300

    conf: application configurations

--- src/main/java/com/cherry/med/CherryMedApplication.java
+++ src/main/java/com/cherry/med/CherryMedApplication.java
@@ -1,4 +1,4 @@
-package br.com.cherrymed.cherrymed;
+package com.cherry.med;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
